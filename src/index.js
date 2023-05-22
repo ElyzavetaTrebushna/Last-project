38630ffb906dcb8a57a0202e9ca1b5fb93e84bfe
ElyzavetaTrebushna <19tess99g@gmail.com>
2023-05-22 17:07:44 +0300

    deleted comments js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,3 @@
-/*let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100
-  },
-  kyiv: {
-    temp: -5,
-    humidity: 20
-  }
-};*/
-
-/*let city = prompt("Enter a city?");
-city = city.toLowerCase();
-if (weather[city] !== undefined) {
-  let temperature = weather[city].temp;
-  let humidity = weather[city].humidity;
-  let celsiusTemperature = Math.round(temperature);
-  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
-
-  alert(
-    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
-  );
-} else {
-  alert(
-    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-  );
-}*/
 
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -47,8 +8,8 @@ function displayWeatherCondition(response) {
   document.querySelector(".humidity").innerHTML=response.data.main.humidity;
   document.querySelector(".wind").innerHTML=Math.round(response.data.wind.speed);
   document.querySelector(".descr_day").innerHTML=response.data.weather[0].main;
-}
-
+  document.querySelector(".icon_today").innerHTML=response.data.weather[0].icon;
+          
 function searchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
